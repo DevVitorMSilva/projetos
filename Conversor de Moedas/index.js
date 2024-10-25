bbd0b77f6f3b38b725f64634b0e6d452fa917e80
Vitor Silva <vitormarcos2005@hotmail.com>
2024-10-25 18:50:24 -0300

    feat: adicionar formatação com pontos ao input de valor

--- Conversor de Moedas/index.js
+++ Conversor de Moedas/index.js
@@ -1,5 +1,12 @@
 const button = document.getElementById('conversion-button')
 const elementResult = document.getElementById('conversion-result')
+const inputAmount = document.getElementById('amount')
+
+inputAmount.addEventListener('input', function() {
+    let value = this.value.replace(/\D/g, '')
+    value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    this.value = value
+})
 
 function Concatenate(a, b) {
     let concatenateResult = (a + "-" + b)
@@ -12,23 +19,23 @@ function Calculate(bid, amount) {
 }
 
 button.addEventListener('click', () => {
-    const inputAmount = document.getElementById('amount')
-    const amount = inputAmount.value
+    const amount = inputAmount.value.replace(/\./g, '')
     inputAmount.value = ""
     const currency1 = document.getElementById("currency-1").value
     const currency2 = document.getElementById("currency-2").value
     let exchangeCurrency = Concatenate(currency1, currency2)
     axios.get(`https://economia.awesomeapi.com.br/json/last/${exchangeCurrency}`)
         .then(response => {
-        const data = response.data;
-        const currencyKey = Object.keys(data)[0];
-        const bid = parseFloat(data[currencyKey].bid)
-        let conversionCalculate = Calculate(bid, amount)
-        elementResult.innerHTML = conversionCalculate.toFixed(2)
-    })
-    .catch(function(error) {
-        elementResult.innerHTML = ('Erro ao fazer a requisição')
-        console.log(error)
-    })
+            const data = response.data;
+            const currencyKey = Object.keys(data)[0]
+            const bid = parseFloat(data[currencyKey].bid)
+            let conversionCalculate = Calculate(bid, amount)
+            elementResult.innerHTML = conversionCalculate.toFixed(2)
+        })
+        .catch(function (error) {
+            elementResult.innerHTML = ('Erro ao fazer a requisição')
+            console.log(error)
+        })
 
 })
+
